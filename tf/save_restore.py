6372ab07b8cc2c46787a9c2c7a83a659eca5649c
wll <iwanglulu@gmail.com>
2017-11-05 00:12:36 +0800

    update tf/..

--- tf/save_restore.py
+++ tf/save_restore.py
@@ -8,51 +8,49 @@ m, n = housing.data.shape
 housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 
 
-n_epochs = 1000
+n_epochs = 1000 
 learning_rate = 0.01
-
-batch_size = 5500
-n_batches = int(np.ceil(m / batch_size))
 
 # transform the data to let all of its fratures' mean=0 and std=1
 transformer = sklearn.preprocessing.StandardScaler().fit(housing_data_plus_bias)
 scaled_housing_data_plus_bias = transformer.transform(housing_data_plus_bias)
 
 # create node and computing graph
-X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X") 
-y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X") 
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
 theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
 y_pred = tf.matmul(X, theta, name="predictions")
 error = y_pred - y
 mse = tf.reduce_mean(tf.square(error), name="mse")      # total loss
 
 
+# use a optimizer
 optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
 training_op = optimizer.minimize(mse)
 
-# fetch_batch should return batch in a random order, for simplicity this demo avoid it.
-def fetch_batch(epoch, batch_index, batch_size):
-    m,_ = scaled_housing_data_plus_bias.shape
 
-    if (batch_index+1)*batch_size > m:
-        to = m
-    else:
-        to = (batch_index+1)*batch_size
-
-    X_batch = scaled_housing_data_plus_bias[batch_index*batch_size:to]
-    y_batch = housing.target.reshape(-1, 1)[batch_index*batch_size:to]
-    return X_batch, y_batch
-
-
+# eval the graph in a session and save all of the parameters
 init = tf.global_variables_initializer()
+saver = tf.train.Saver()    # create a saver
+
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(n_epochs):
-        for batch_index in range(n_batches):
-            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
-            sess.run(training_op, feed_dict={X:X_batch, y:y_batch})
-            print("epoch", epoch, "batches", batch_index, " MSE=", mse.eval(feed_dict={X:X_batch, y:y_batch}))
-    
-    best_theta = theta.eval()
 
-print(best_theta)
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("epoch:", epoch, " MSE=", mse.eval())
+            save_path = saver.save(sess, "/tmp/my_model.ckpt")
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+    save_path = saver.save(sess, "/tmp/my_model_final.ckpt")
+
+print("best theta:", best_theta)
+
+
+# restoring all the parameters in asession
+with tf.Session() as sess:
+    saver.restore(sess,"/tmp/my_model_final.ckpt")
+    restored_best_theta = theta.eval()
+
+print("restored best theta:", restored_best_theta)
